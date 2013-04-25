242cec51613d7d9a4e194b6bda8fa060cc80106f
Benno Leslie <benno@breakawayconsulting.com.au>
2013-04-25 16:40:06 +0800

    Add facility for adding extra files to a release config.

--- x.py
+++ x.py
@@ -1097,6 +1097,7 @@ class Release(metaclass=ReleaseMeta):
     enabled = False
     license = None
     top_level_license = None
+    extra_files = []
 
 
 def get_release_configs():
@@ -1126,6 +1127,9 @@ def build_single_release(config):
             tar_add_data(tf, '{}/LICENSE'.format(basename),
                          config.top_level_license.encode('utf8'),
                          tar_info_filter)
+
+        for arcname, filename in config.extra_files:
+            tf.add(filename, arcname='{}/{}'.format(basename, arcname), filter=tar_info_filter)
 
         if 'TEAMCITY_VERSION' in os.environ:
             build_info = os.environ['BUILD_VCS_NUMBER']

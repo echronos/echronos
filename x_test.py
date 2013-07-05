71aaf57daf5acd95bd104e47fe3d312a851098ff
Benno Leslie <benno@breakawayconsulting.com.au>
2013-07-05 14:25:17 +1000

    Ensure x tests work correctly on TEAMCITY.
    On TEAMCITY there is no 'git' repo when the tests are run, so
    avoid running git related unit tests on TeamCity.
    
    This isn't ideal; ideally TeamCity would have a way to access
    git, however this isn't critical, and x.py is only an internal
    tool, so if the git support breaks it will not easily go unnoticed.

--- x_test.py
+++ x_test.py
@@ -1,4 +1,5 @@
 from x import *
+import os
 
 # The constants refer to the base (initial) commit.
 # All branches should be dervied from this commit, so it
@@ -7,17 +8,18 @@ from x import *
 INITIAL_COMMIT = '052c07259121ae27a0736dfe92cd5b072ecc5745'
 INITIAL_TIME = 1364960632
 
+IN_TEAMCITY = 'TEAMCITY_PROJECT_NAME' in os.environ
 
 def test_empty():
     """Test whether an empty test can be run at all given the test setup in x.py."""
     pass
 
+if not IN_TEAMCITY:
+    def test_git_branch_hash():
+        g = Git()
+        assert INITIAL_COMMIT == g.branch_hash(INITIAL_COMMIT)
 
-def test_git_branch_hash():
-    g = Git()
-    assert INITIAL_COMMIT == g.branch_hash(INITIAL_COMMIT)
 
-
-def test_git_branch_date():
-    g = Git()
-    assert INITIAL_TIME == g.branch_date(INITIAL_COMMIT)
+    def test_git_branch_date():
+        g = Git()
+        assert INITIAL_TIME == g.branch_date(INITIAL_COMMIT)

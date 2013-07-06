d1b7956d1a1d79102b5d239331a59e8adeeda01e
Benno Leslie <benno@breakawayconsulting.com.au>
2013-07-06 18:40:28 +1000

    Add support for building shared library with the POSIX builder.

--- packages/posix/build.py
+++ packages/posix/build.py
@@ -1,14 +1,26 @@
 from prj import execute, SystemBuildError
 
 
+schema = {
+    'type': 'dict',
+    'name': 'module',
+    'dict_type':
+    [
+        {'type': 'string', 'name': 'output_type', 'default': 'executable'},
+    ]
+}
+
+
 def run(system, configuration=None):
-    return system_build(system)
+    return system_build(system, configuration)
 
 
-def system_build(system):
+def system_build(system, configuration):
     inc_path_args = ['-I%s' % i for i in system.include_paths]
 
     if len(system.c_files) == 0:
         raise SystemBuildError("Zero C files in system definition")
 
-    execute(['gcc', '-o', system.output_file, '-Wall', '-Werror'] + inc_path_args + system.c_files)
+    shared_args = ['-shared'] if configuration['output_type'] == 'shared-library' else []
+
+    execute(['gcc', '-o', system.output_file, '-Wall', '-Werror'] + shared_args + inc_path_args + system.c_files)

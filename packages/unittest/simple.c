450c551edf2908f8af5715c6cb9343856d015367
Benno Leslie <benno@breakawayconsulting.com.au>
2013-07-06 18:47:19 +1000

    Add a simple unit and system for testing the unit.

---
+++ packages/unittest/simple.c
@@ -0,0 +1,12 @@
+/* The simple module is primarily used to sanity check the unit test framework itself, rather than any code. */
+int
+foo(void)
+{
+    return 37;
+}
+
+int
+add(int a, int b)
+{
+    return a + b;
+}

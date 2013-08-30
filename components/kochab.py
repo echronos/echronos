99ca1bd1a244be52812f5ea88c9c8b4bdc70af45
Benno Leslie <benno@breakawayconsulting.com.au>
2013-08-30 17:24:37 +1000

    Add priority argument and sorting of priorities.

--- components/kochab.py
+++ components/kochab.py
@@ -1,4 +1,5 @@
 from prj import Module
+from operator import itemgetter
 
 
 class KochabModule(Module):
@@ -8,9 +9,10 @@ class KochabModule(Module):
    <entry name="signalset_size" type="int" default="8"/>
    <entry name="irqeventid_size" type="int" default="8"/>
    <entry name="prefix" type="c_ident" default="rtos_" />
-   <entry name="tasks" type="list" auto_index_field="idx">
+   <entry name="tasks" type="list">
      <entry name="task" type="dict">
       <entry name="entry" type="c_ident" />
+      <entry name="priority" type="int" />
       <entry name="name" type="ident" />
       <entry name="stack_size" type="int" />
      </entry>
@@ -43,5 +45,14 @@ class KochabModule(Module):
         {'input': 'rtos-kochab.h', 'render': True},
         {'input': 'rtos-kochab.c', 'render': True, 'type': 'c'},
     ]
+
+    def configure(self, xml_config):
+        config = super().configure(xml_config)
+
+        tasks = config['tasks']
+        tasks.sort(key=itemgetter('priority'), reverse=True)
+        for idx, t in enumerate(tasks):
+            t['idx'] = idx
+        return config
 
 module = KochabModule()

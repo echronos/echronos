a2b9a1c5a59c721477838737dffbcc3dc4d22517
Stefan Goetz <stg@breakawayconsulting.com.au>
2013-11-07 10:15:01 +1000

    Add comment to ensure code coverage by example systems for regression testing.

--- components/profiling.c
+++ components/profiling.c
@@ -46,6 +46,11 @@ static uint32_t profiling_task_uptimes[{{tasks.length}} + 1];
 /*| public_functions |*/
 {{#profiling}}
 
+/* MAINTENANCE:
+ * When modifying or extending the profiling component, ensure that its code is covered by at least one of the example
+ * systems to ensure it builds correctly.
+ */
+
 void
 {{prefix_func}}profiling_record_sample(void)
 {

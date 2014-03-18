de63fb21e5e33fba6a0bdbeaf85c799065fbd485
Stefan Goetz <stg@breakawayconsulting.com.au>
2014-03-19 07:28:41 +1000

    Add missing declaration of function timer_error_set()

--- components/timer.c
+++ components/timer.c
@@ -26,6 +26,7 @@ void {{prefix_func}}timer_oneshot({{prefix_type}}TimerId timer_id, {{prefix_type
 bool {{prefix_func}}timer_check_overflow({{prefix_type}}TimerId timer_id);
 {{prefix_type}}TicksRelative {{prefix_func}}timer_remaining({{prefix_type}}TimerId timer_id);
 void {{prefix_func}}timer_reload_set({{prefix_type}}TimerId timer_id, {{prefix_type}}TicksRelative reload);
+void {{prefix_func}}timer_error_set({{prefix_type}}TimerId timer_id, {{prefix_type}}ErrorId error_id);
 void {{prefix_func}}timer_signal_set({{prefix_type}}TimerId timer_id, {{prefix_type}}TaskId task_id, {{prefix_type}}SignalSet signal_set);
 {{/timers.length}}
 

95cee8b218006a129e86f28c4adc10b7dc7030ae
Stefan Goetz <stg@breakawayconsulting.com.au>
2014-04-30 07:14:15 +1000

    Create task component from task-related code that is common to all RTOS variants

---
+++ components/task.c
@@ -0,0 +1,69 @@
+/*| public_headers |*/
+#include <stdint.h>
+
+/*| public_type_definitions |*/
+typedef uint{{taskid_size}}_t {{prefix_type}}TaskId;
+
+/*| public_structure_definitions |*/
+
+/*| public_object_like_macros |*/
+#define {{prefix_const}}TASK_ID_ZERO (({{prefix_type}}TaskId) UINT{{taskid_size}}_C(0))
+#define {{prefix_const}}TASK_ID_MAX (({{prefix_type}}TaskId)UINT{{taskid_size}}_C({{tasks.length}} - 1))
+{{#tasks}}
+#define {{prefix_const}}TASK_ID_{{name|u}} (({{prefix_type}}TaskId) UINT{{taskid_size}}_C({{idx}}))
+{{/tasks}}
+
+/*| public_function_like_macros |*/
+
+/*| public_extern_definitions |*/
+
+/*| public_function_definitions |*/
+
+/*| headers |*/
+#include <stdint.h>
+
+/*| object_like_macros |*/
+#define TASK_ID_NONE ((TaskIdOption) UINT{{taskid_size}}_MAX)
+
+/*| type_definitions |*/
+typedef {{prefix_type}}TaskId TaskIdOption;
+
+/*| structure_definitions |*/
+struct task
+{
+    context_t ctx;
+};
+
+/*| extern_definitions |*/
+{{#tasks}}
+extern void {{function}}(void);
+{{/tasks}}
+
+/*| function_definitions |*/
+
+/*| state |*/
+static {{prefix_type}}TaskId current_task;
+static struct task tasks[{{tasks.length}}];
+
+/*| function_like_macros |*/
+{{#internal_asserts}}
+#define get_current_task() get_current_task_check()
+{{/internal_asserts}}
+{{^internal_asserts}}
+#define get_current_task() current_task
+{{/internal_asserts}}
+#define get_task_context(task_id) &tasks[task_id].ctx
+#define internal_assert_task_valid(task) internal_assert(task < {{tasks.length}}, ERROR_ID_INTERNAL_INVALID_ID)
+#define assert_task_valid(task) api_assert(task < {{tasks.length}}, ERROR_ID_INVALID_ID)
+
+/*| functions |*/
+{{#internal_asserts}}
+static {{prefix_type}}TaskId
+get_current_task_check(void)
+{
+    internal_assert(current_task < {{tasks.length}}, ERROR_ID_INTERNAL_CURRENT_TASK_INVALID);
+    return current_task;
+}
+{{/internal_asserts}}
+
+/*| public_functions |*/

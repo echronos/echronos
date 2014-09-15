6ee707e84d31cd65840405fd0e483eb9b60e1956
Robert Sison <Robert.Sison@nicta.com.au>
2014-09-15 13:31:49 +1000

    Decouple ppce500 vectable package from linker script because they're not interdependent

---
+++ packages/ppce500/default-linker.py
@@ -0,0 +1,17 @@
+from prj import Module
+import logging
+logger = logging.getLogger()
+
+
+class DefaultLinkerModule(Module):
+    # Default stack size value is arbitrary.
+    xml_schema = """
+<schema>
+    <entry name="stack_size" type="int" default="0x1000" />
+</schema>"""
+
+    files = [
+        {'input': 'default.ld', 'render': True, 'type': 'linker_script', 'stage': 'post_prepare'},
+    ]
+
+module = DefaultLinkerModule()
--- packages/ppce500/vectable.s
+++ packages/ppce500/vectable.s
@@ -1,3 +1,37 @@
+/*<module>
+  <code_gen>template</code_gen>
+  <schema>
+    <entry name="critical_input" type="c_ident" default="" />
+    <entry name="machine_check" type="c_ident" default="" />
+    <entry name="data_storage" type="c_ident" default="" />
+    <entry name="instruction_storage" type="c_ident" default="" />
+    <entry name="external_input" type="c_ident" default="" />
+    <entry name="alignment" type="c_ident" default="" />
+    <entry name="program" type="c_ident" default="" />
+    <entry name="floating_point" type="c_ident" default="" />
+    <entry name="system_call" type="c_ident" default="" />
+    <entry name="aux_processor" type="c_ident" default="" />
+    <entry name="decrementer" type="c_ident" default="" />
+    <entry name="fixed_interval_timer" type="c_ident" default="" />
+    <entry name="watchdog_timer" type="c_ident" default="" />
+    <entry name="data_tlb_error" type="c_ident" default="" />
+    <entry name="instruction_tlb_error" type="c_ident" default="" />
+    <entry name="debug" type="c_ident" default="" />
+    <entry name="eis_spe_apu" type="c_ident" default="" />
+    <entry name="eis_fp_data" type="c_ident" default="" />
+    <entry name="eis_fp_round" type="c_ident" default="" />
+    <entry name="eis_perf_monitor" type="c_ident" default="" />
+  </schema>
+</module>*/
+
+/*
+ * For interrupt handlers an empty string (the default) will simply generate a vector that loops forever on itself.
+ * Alternatively, setting the handler to "undefined" will generate a handler that first creates a stack frame for the
+ * interrupted context and stores its registers there before looping forever at location "undefined".
+ * Apart from the few handlers whose handler-wrappers (defined in this file) clear their relevant MSR bits, the given
+ * handler must be responsible for clearing the condition that caused its interrupt.
+ */
+
 .section .vectors, "a"
 /* This is here to catch unconfigured interrupts, or any other (deliberate or erroneous) jumps to address NULL. */
 undefined:

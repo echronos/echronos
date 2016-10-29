334ba00933208358b305f9282d56612a179431a2
Stefan Götz <stg@brkawy.com>
2016-10-29 20:29:02 +1000

    To ensure that unit test systems can be built successfully on Windows, disable the '-fPIC' compiler option on that platform because it is implicit and therefore generates a compiler warning which leads to build failures.

--- packages/posix/build.py
+++ packages/posix/build.py
@@ -25,6 +25,7 @@
 # @TAG(NICTA_AGPL)
 #
 
+import sys
 from prj import execute, SystemBuildError
 
 
@@ -45,6 +46,11 @@ def system_build(system, configuration):
     if len(system.c_files) == 0:
         raise SystemBuildError("Zero C files in system definition")
 
-    shared_args = ['-shared', '-fPIC', '-std=c90'] if configuration['output_type'] == 'shared-library' else []
+    if configuration['output_type'] == 'shared-library':
+        shared_args = ['-shared']
+        if sys.platform != 'win32':
+            shared_args.append('-fPIC')
+    else:
+        shared_args = []
 
-    execute(['gcc', '-o', system.output_file, '-Wall', '-Werror'] + shared_args + inc_path_args + system.c_files)
+    execute(['gcc', '-o', system.output_file, '-Wall', '-Werror', '-std=c90'] + shared_args + inc_path_args + system.c_files)

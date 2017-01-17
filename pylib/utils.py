78f1d922bd384c3bba76c7e4905584ca39928ea9
Stefan Götz <stg@brkawy.com>
2017-01-17 15:35:14 +1100

    - re-enable too-many-public-methods pylint check globally and disable it locally

--- pylib/utils.py
+++ pylib/utils.py
@@ -242,6 +242,7 @@ def get_executable_extension():
     return _EXECUTABLE_EXTENSION
 
 
+# pylint: disable=too-many-public-methods
 class Git:
     """
     Represents common state applicable to a series of git invocations and provides a pythonic interface to git.

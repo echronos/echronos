3912a419eff24cb9a54520ab1a949f5061babee2
Stefan Götz <stg@brkawy.com>
2017-01-29 16:09:58 +1000

    - re-enable the undefined-variable pylint check globally and fix uses of undefined variables

--- pylib/task.py
+++ pylib/task.py
@@ -190,7 +190,7 @@ class Task:
                 break
         else:
             raise FileNotFoundError('Unable to determine review round for task "{}" and reviewer "{}" ("{}")'
-                                    .format(branch, reviewer, review_path_template))
+                                    .format(self.name, reviewer, review_path_template))
 
         if not accept:
             review_template = """Reviewer: {} ({})

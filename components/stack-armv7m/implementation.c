6c4701fb2b1856f121c3a9a43d0dd186617322c9
Sebastian Holzapfel <seb.holzapfel@data61.csiro.au>
2017-04-12 14:20:10 +1000

    Force stack into known, aligned section so that it can be protected by the MPU

--- components/stack-armv7m/implementation.c
+++ components/stack-armv7m/implementation.c
@@ -13,8 +13,20 @@
 /*| function_declarations |*/
 
 /*| state |*/
+{{#memory_protection}}
+/* We need to align stack addresses on their size to create a valid MPU region.
+ * Additionally, we place the stack in a specially named section so that
+ * we are able to see where the linker places it and configure the MPU accordingly. */
+{{/memory_protection}}
 {{#tasks}}
+{{#memory_protection}}
+static uint32_t stack_{{idx}}[{{stack_size}}]
+    __attribute__(( aligned({{stack_size}}*sizeof(uint32_t)) ))
+    __attribute__(( section (".bss.stack.task_{{idx}}") ));
+{{/memory_protection}}
+{{^memory_protection}}
 static uint32_t stack_{{idx}}[{{stack_size}}] __attribute__((aligned(8)));
+{{/memory_protection}}
 {{/tasks}}
 
 /*| function_like_macros |*/
@@ -22,3 +34,5 @@ static uint32_t stack_{{idx}}[{{stack_size}}] __attribute__((aligned(8)));
 /*| functions |*/
 
 /*| public_functions |*/
+
+/*| public_privileged_functions |*/

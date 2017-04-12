ea685d655dcb6d30edf58eebd3fe0de192ecd836
Sebastian Holzapfel <seb.holzapfel@data61.csiro.au>
2017-04-12 14:27:20 +1000

    Add armv7m build script that will allow building MPU systems

---
+++ packages/armv7m/build-mpu.py
@@ -0,0 +1,59 @@
+#
+# eChronos Real-Time Operating System
+# Copyright (C) 2015  National ICT Australia Limited (NICTA), ABN 62 102 206 173.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3, provided that these additional
+# terms apply under section 7:
+#
+#   No right, title or interest in or to any trade mark, service mark, logo or
+#   trade name of of National ICT Australia Limited, ABN 62 102 206 173
+#   ("NICTA") or its licensors is granted. Modified versions of the Program
+#   must be plainly marked as such, and must not be distributed using
+#   "eChronos" as a trade mark or product name, or misrepresented as being the
+#   original Program.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @TAG(NICTA_AGPL)
+#
+
+from prj import execute, SystemBuildError
+import os
+
+
+def run(system, configuration=None):
+    return system_build(system)
+
+
+def system_build(system):
+    inc_path_args = ['-I%s' % i for i in system.include_paths]
+    common_flags = ['-mthumb', '-march=armv7-m', '-g']
+    a_flags = common_flags
+    c_flags = common_flags + ['-O0', '-fdata-sections', '-fno-common', '-fno-zero-initialized-in-bss']
+    ld_flags = ['--print-memory-usage']
+
+    # Compile all C files.
+    c_obj_files = [os.path.join(system.output, os.path.basename(c.replace('.c', '.o'))) for c in system.c_files]
+
+    for c, o in zip(system.c_files, c_obj_files):
+        os.makedirs(os.path.dirname(o), exist_ok=True)
+        execute(['arm-none-eabi-gcc', '-ffreestanding', '-c', c, '-o', o, '-Wall', '-Werror'] +
+                c_flags + inc_path_args)
+
+    # Assemble all asm files.
+    asm_obj_files = [os.path.join(system.output, os.path.basename(s.replace('.s', '.o'))) for s in system.asm_files]
+    for s, o in zip(system.asm_files, asm_obj_files):
+        os.makedirs(os.path.dirname(o), exist_ok=True)
+        execute(['arm-none-eabi-as', '-o', o, s] + a_flags + inc_path_args)
+
+    # Perform final link
+    obj_files = asm_obj_files + c_obj_files
+    execute(['arm-none-eabi-ld', '-T', system.linker_script, '-o', system.output_file] + ld_flags + obj_files)

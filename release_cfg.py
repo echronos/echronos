12566547e9289fdf0ee3cdc359e101cf5f34e48c
Stefan Götz <stg@brkawy.com>
2017-04-19 15:21:59 +1000

    Fix pycodestyle invocation and test failures Update patch number of release version. It is necessary to reflect that the task fix_pycodestyle_invocation_and_test_failures introduces changes. It is sufficient because the changes in task fix_pycodestyle_invocation_and_test_failures do not impact existing users at all because they are, as far as release functionality is concerned, non-functional changes.

--- release_cfg.py
+++ release_cfg.py
@@ -32,7 +32,7 @@ class Standard(Release):
     packages = ['armv7m', 'generic', 'rtos-example', 'machine-qemu-simple', 'machine-stm32f4-discovery',
                 'machine-armv7m-common']
     platforms = ['x86_64-apple-darwin', 'x86_64-unknown-linux-gnu']
-    version = '1.1.1'
+    version = '1.1.2'
     product_name = 'eChronos'
     release_name = 'std'
     enabled = True

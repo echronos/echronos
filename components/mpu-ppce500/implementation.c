3119875f76f710ddc60c6f8ffc2eb21b1670d293
Sebastian Holzapfel <seb.holzapfel@data61.csiro.au>
2017-05-11 11:33:28 +1000

    Add empty mpu implementations for other platforms so their systems remain buildable

---
+++ components/mpu-ppce500/implementation.c
@@ -0,0 +1,21 @@
+/*| headers |*/
+
+/*| object_like_macros |*/
+
+/*| types |*/
+
+/*| structures |*/
+
+/*| extern_declarations |*/
+
+/*| function_declarations |*/
+
+/*| state |*/
+
+/*| function_like_macros |*/
+
+/*| functions |*/
+
+/*| public_functions |*/
+
+/*| public_privileged_functions |*/

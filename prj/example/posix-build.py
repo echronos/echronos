af7d8c6c94b23a798808b211b4d59f1101f40087
Stefan Götz <stg@brkawy.com>
2017-06-06 19:02:02 +1000

    Resolve code style issues in files now covered by pylint.

--- prj/example/posix-build.py
+++ prj/example/posix-build.py
@@ -35,10 +35,10 @@ def system_build(output_file, modules, include_paths=None):
     inc_path_args = ['-I%s' % i for i in include_paths]
 
     c_files = []
-    for m in modules:
-        c_files.extend(m.c_files())
+    for mod in modules:
+        c_files.extend(mod.c_files())
 
-    if len(c_files) == 0:
+    if not c_files:
         raise SystemBuildError("Zero C files in system definition")
 
     execute(['gcc', '-o', output_file, '-Wall'] + inc_path_args + c_files)

6de9883157e236f97d76c48a3d78d6643fe217aa
Sebastian Holzapfel <seb.holzapfel@data61.csiro.au>
2017-06-23 10:35:00 +1000

    Update mpu build script to adhere to new style checks

--- packages/machine-stm32f4-discovery/build_mpu.py
+++ packages/machine-stm32f4-discovery/build_mpu.py
@@ -25,17 +25,17 @@
 # @TAG(NICTA_AGPL)
 #
 
-from prj import execute, SystemBuildError
 import os
+from prj import execute, commonpath
 
 
-def run(system, configuration=None):
+def run(system, _=None):
     return system_build(system)
 
 
 def system_build(system):
     inc_path_args = ['-I%s' % i for i in system.include_paths]
-    common_flags = ['-mthumb', '-march=armv7-m', '-g']
+    common_flags = ['-mthumb', '-g3', '-mlittle-endian', '-mcpu=cortex-m4', '-mfloat-abi=hard', '-mfpu=fpv4-sp-d16']
     a_flags = common_flags
     c_flags = common_flags + ['-O0', '-fdata-sections', '-fno-common', '-fno-zero-initialized-in-bss']
 
@@ -43,19 +43,26 @@ def system_build(system):
     # memory-constrained, but only works with recent versions of ld
     ld_flags = []
 
-    # Compile all C files.
-    c_obj_files = [os.path.join(system.output, os.path.basename(c.replace('.c', '.o'))) for c in system.c_files]
+    all_input_files = system.c_files + system.asm_files
+    all_input_files = [os.path.normpath(os.path.abspath(path)) for path in all_input_files]
+    common_parent_path = commonpath(all_input_files)
 
-    for c, o in zip(system.c_files, c_obj_files):
-        os.makedirs(os.path.dirname(o), exist_ok=True)
-        execute(['arm-none-eabi-gcc', '-ffreestanding', '-c', c, '-o', o, '-Wall', '-Werror'] +
+    # Compile all C files.
+    c_obj_files = [os.path.join(system.output, os.path.relpath(os.path.abspath(c_file_path.replace('.c', '.o')),
+                                                               common_parent_path)) for c_file_path in system.c_files]
+
+    for c_path, obj_path in zip(system.c_files, c_obj_files):
+        os.makedirs(os.path.dirname(obj_path), exist_ok=True)
+        execute(['arm-none-eabi-gcc', '-ffreestanding', '-c', c_path, '-o', obj_path, '-Wall', '-Werror'] +
                 c_flags + inc_path_args)
 
     # Assemble all asm files.
-    asm_obj_files = [os.path.join(system.output, os.path.basename(s.replace('.s', '.o'))) for s in system.asm_files]
-    for s, o in zip(system.asm_files, asm_obj_files):
-        os.makedirs(os.path.dirname(o), exist_ok=True)
-        execute(['arm-none-eabi-as', '-o', o, s] + a_flags + inc_path_args)
+    asm_obj_files = [os.path.join(system.output, os.path.relpath(os.path.abspath(asm_file_path.replace('.s', '.o')),
+                                                                 common_parent_path))
+                     for asm_file_path in system.asm_files]
+    for asm_file_path, obj_file_path in zip(system.asm_files, asm_obj_files):
+        os.makedirs(os.path.dirname(obj_file_path), exist_ok=True)
+        execute(['arm-none-eabi-as', '-o', obj_file_path, asm_file_path] + a_flags + inc_path_args)
 
     # Perform final link
     obj_files = asm_obj_files + c_obj_files
